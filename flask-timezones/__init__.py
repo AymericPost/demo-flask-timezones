0a1bd3d089d6a80d207652ff7a3999ad8aa091a4
AymericPost <aymeric.post@gmail.com>
2021-03-22 16:29:28 +0100

    All output JSONified

--- flask-timezones/__init__.py
+++ flask-timezones/__init__.py
@@ -11,11 +11,18 @@ def create_app():
             (gmt.replace("-", "+"), gmt.replace("+", "-"))[gmt.startswith("+")]
         )
 
+        tzForGmt = []
+
         if(tz in all_timezones):
+            current = datetime.now( timezone(tz) ).strftime("%d/%m/%Y %H:%M:%S")
 
-            time = datetime.now( timezone(tz) )
+            for one_timezone in all_timezones:
+                time = datetime.now( timezone(one_timezone) ).strftime("%d/%m/%Y %H:%M:%S")
 
-            return time.strftime("%d/%m/%Y %H:%M:%S")
+                if("Etc" not in one_timezone and "/" in one_timezone and current == time):
+                    tzForGmt.append(one_timezone)
+
+            return jsonify({"timezone": "GMT{}".format(gmt),"time": current, "locales": tzForGmt})
         else:
             return jsonify({"error": "BAD REQUEST", "message": "Fuseau horaire incorrect."}), 400
     
